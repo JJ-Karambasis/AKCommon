ac9a7265a5e74095f6cb923b3cc5593b50411bea
Karmatix <akarambasis@gmail.com>
2020-08-20 21:12:07 -0400

    Added some file io to platform

--- ak_platform.h
+++ ak_platform.h
@@ -1,7 +1,46 @@
 #ifndef AK_PLATFORM_H
 #define AK_PLATFORM_H
 
+enum platform_file_attributes
+{
+    PLATFORM_FILE_ATTRIBUTES_READ,
+    PLATFORM_FILE_ATTRIBUTES_WRITE
+};
+
+global const char* __Global__Platform_Internal_ErrorMessage;
+
+inline b32 Platform_Error(const char* ErrorMessage)
+{
+    __Global__Platform_Internal_ErrorMessage = ErrorMessage;
+    return false;
+}
+
+inline const char* Platform_GetErrorMessage()
+{
+    return __Global__Platform_Internal_ErrorMessage;
+}
+
+#ifdef INVALID_FILE_SIZE
+#undef INVALID_FILE_SIZE
+#endif
+
+#define INVALID_FILE_SIZE ((u64)-1)
+#define NO_OFFSET ((u64)-1)
+
 #ifdef OS_WINDOWS
+
+typedef u64 platform_time;
+
+struct platform_file_handle
+{
+    HANDLE Handle;
+    platform_file_attributes Attributes;
+    
+    b32 IsValid()
+    {
+        return (Handle != NULL) && (Handle != INVALID_HANDLE_VALUE);
+    }
+};
 
 inline void* 
 AllocateMemory(ptr Size)
@@ -31,6 +70,123 @@ FileExists(char* Path)
 {
     DWORD Attrib = GetFileAttributes(Path);
     b32 Result = (Attrib != INVALID_FILE_ATTRIBUTES) && !(Attrib & FILE_ATTRIBUTE_DIRECTORY);
+    return Result;
+}
+
+inline platform_file_handle
+FileOpen(char* Path, platform_file_attributes Attributes)
+{
+    DWORD DesiredAttributes = 0;
+    DWORD CreationDisposition = 0;
+    if(Attributes == PLATFORM_FILE_ATTRIBUTES_READ)
+    {
+        DesiredAttributes = GENERIC_READ;
+        CreationDisposition = OPEN_EXISTING;
+    }
+    else if(Attributes == PLATFORM_FILE_ATTRIBUTES_WRITE)
+    {
+        DesiredAttributes = GENERIC_WRITE;
+        CreationDisposition = CREATE_ALWAYS;
+    }
+    else
+    {
+        Platform_Error("Invalid platform file attribute");                
+        return {};
+    }    
+    
+    HANDLE Handle = CreateFile(Path, DesiredAttributes, 0, NULL, CreationDisposition, FILE_ATTRIBUTE_NORMAL, NULL);
+    if(Handle == INVALID_HANDLE_VALUE)
+    {
+        Platform_Error("Failed to open the file.");
+        return {};
+    }
+    
+    platform_file_handle Result = {};
+    Result.Handle = Handle;
+    Result.Attributes = Attributes;
+    return Result;
+}
+
+inline void
+FileClose(platform_file_handle* Handle)
+{
+    ASSERT(Handle->IsValid());
+    CloseHandle(Handle->Handle);
+    *Handle = {};
+}
+
+inline u64 
+FileGetSize(platform_file_handle* Handle)
+{
+    ASSERT(Handle->IsValid() && (Handle->Attributes == PLATFORM_FILE_ATTRIBUTES_READ));
+    
+    LARGE_INTEGER Result;
+    if(!GetFileSizeEx(Handle->Handle, &Result))
+    {
+        Platform_Error("Failed to get file size.");
+        return INVALID_FILE_SIZE;    
+    }
+    return Result.QuadPart;
+}
+
+inline b32
+FileRead(platform_file_handle* Handle, void* Data, u32 ReadSize, u64 Offset)
+{
+    ASSERT(Handle->IsValid() && (Handle->Attributes == PLATFORM_FILE_ATTRIBUTES_READ));
+    
+    OVERLAPPED* OffsetPointer = NULL;
+    OVERLAPPED Offsets = {};
+    if(Offset != NO_OFFSET)
+    {
+        Offsets.Offset = (DWORD)(Offset & 0xFFFFFFFF);
+        Offsets.OffsetHigh = (DWORD)((Offset >> 32) & 0xFFFFFFFF);
+        OffsetPointer = &Offsets;
+    }
+    
+    DWORD BytesRead;
+    if(ReadFile(Handle->Handle, Data, ReadSize, &BytesRead, OffsetPointer) && (BytesRead == ReadSize))
+        return true;
+    
+    Platform_Error("Failed to read the file.");    
+    return false;
+}
+
+inline b32
+FileWrite(platform_file_handle* Handle, void* Data, u32 WriteSize, u64 Offset)
+{
+    ASSERT(Handle->IsValid() && (Handle->Attributes == PLATFORM_FILE_ATTRIBUTES_WRITE));
+    
+    OVERLAPPED* OffsetPointer = NULL;
+    OVERLAPPED Offsets = {};
+    if(Offset != NO_OFFSET)
+    {
+        Offsets.Offset = (DWORD)(Offset & 0xFFFFFFFF);
+        Offsets.OffsetHigh = (DWORD)((Offset >> 32) & 0xFFFFFFFF);
+        OffsetPointer = &Offsets;
+    }
+    
+    DWORD BytesWritten;
+    if(WriteFile(Handle->Handle, Data, WriteSize, &BytesWritten, OffsetPointer) && (BytesWritten == WriteSize))
+        return true;
+    
+    Platform_Error("Failed to write the file.");
+    return false;
+}
+
+global b32 _Internal_Global_InitFrequency_;
+global LARGE_INTEGER _Internal_Global_Frequency_;
+
+inline platform_time WallClock()
+{
+    LARGE_INTEGER Result;
+    QueryPerformanceCounter(&Result);
+    return Result.QuadPart;
+}
+
+inline f64 GetElapsedTime(platform_time End, platform_time Start)
+{
+    if(!_Internal_Global_InitFrequency_) { QueryPerformanceFrequency(&_Internal_Global_Frequency_); _Internal_Global_InitFrequency_ = true; }    
+    f64 Result = ((f64)(End-Start))/((f64)_Internal_Global_Frequency_.QuadPart);
     return Result;
 }
 
@@ -78,24 +234,59 @@ FileRename(string OldName, string NewName)
     return Result;
 }
 
-inline buffer
-ReadEntireFile(char* Path, arena* Arena = GetDefaultArena())
+inline buffer 
+ReadEntireFile(char* Path)
 {
-    FILE* File = fopen(Path, "rb");
-    if(!File)
+    platform_file_handle File = FileOpen(Path, PLATFORM_FILE_ATTRIBUTES_READ);
+    if(!File.IsValid())
         return {};
     
-    fseek(File, 0, SEEK_END);
-    u32 FileSize = ftell(File);
-    fseek(File, 0, SEEK_SET);
-    
-    void* Data = PushSize(Arena, FileSize, NoClear, 0);
-    fread(Data, FileSize, 1, File);
+    u32 FileSize = SafeU32(FileGetSize(&File));
+    void* Data = AllocateMemory(FileSize);    
+    FileRead(&File, Data, FileSize, NO_OFFSET);
     
     buffer Result;
     Result.Data = (u8*)Data;
     Result.Size = (ptr)FileSize;
+    
+    FileClose(&File);
     return Result;    
+}
+
+inline buffer 
+ReadEntireFile(string Path)
+{
+    return ReadEntireFile(Path.Data);
+}
+
+inline b32
+WriteEntireFile(char* Path, void* Data, u32 DataSize)
+{
+    platform_file_handle File = FileOpen(Path, PLATFORM_FILE_ATTRIBUTES_WRITE);
+    if(!File.IsValid())
+        return false;
+    
+    FileWrite(&File, Data, DataSize, NO_OFFSET);
+    FileClose(&File);
+    return true;
+}
+
+inline b32
+WriteEntireFile(string Path, void* Data, u32 DataSize)
+{
+    return WriteEntireFile(Path.Data, Data, DataSize);
+}
+
+inline void FreeFileMemory(buffer* Buffer)
+{
+    if(Buffer->Data)
+        FreeMemory(Buffer->Data);
+}
+
+inline platform_file_handle
+FileOpen(string Path, platform_file_attributes Attributes)
+{
+    return FileOpen(Path.Data, Attributes);
 }
 
 #endif
